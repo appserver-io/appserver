95f53cf9a4dc4d96b28329f1756dbf401617fc4a
wagnert <t.wagner@techdivision.com>
2013-11-11 22:41:32 +0100

    Raise usleep from 1000 to 10000 microseconds

---
+++ src/TechDivision/ApplicationServer/Stream/SecureReceiver.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * TechDivision\ApplicationServer\Stream\SecureReceiver
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ */
+namespace TechDivision\ApplicationServer\Stream;
+
+use TechDivision\ApplicationServer\AbstractReceiver;
+
+/**
+ *
+ * @package TechDivision\ApplicationServer
+ * @copyright Copyright (c) 2013 <info@techdivision.com> - TechDivision GmbH
+ * @license http://opensource.org/licenses/osl-3.0.php
+ *          Open Software License (OSL 3.0)
+ * @author Johann Zelger <jz@techdivision.com>
+ */
+class SecureReceiver extends AbstractReceiver
+{
+
+    /**
+     * @see \TechDivision\ApplicationServer\AbstractReceiver
+     */
+    protected function getResourceClass()
+    {
+        return 'TechDivision\Stream\SecureServer';
+    }
+}
