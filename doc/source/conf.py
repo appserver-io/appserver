ff42812e3527f02f6148cd5fe025838494ada4e6
Johann Zelger <zelger@me.com>
2014-02-12 18:13:06 +0100

    added deployment.rst temp. to exclude

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -67,7 +67,7 @@ release = '0.1.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['deployment.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
