719a672336f90eebb7d029272db00ee709f4a1b7
Johann Zelger <zelger@me.com>
2014-03-12 15:43:17 +0100

    added virtual host api nodes

---
+++ src/TechDivision/ApplicationServer/Api/Node/VirtualHostNode.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * TechDivision\ApplicationServer\Api\Node\VirtualHostNode
+ *
+ * PHP version 5
+ *
+ * @category   Appserver
+ * @package    TechDivision_ApplicationServer
+ * @subpackage Api
+ * @author     Tim Wagner <tw@techdivision.com>
+ * @author     Johann Zelger <jz@techdivision.com>
+ * @copyright  2013 TechDivision GmbH <info@techdivision.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link       http://www.appserver.io
+ */
+
+namespace TechDivision\ApplicationServer\Api\Node;
+
+/**
+ * DTO to transfer virtual host information.
+ *
+ * @category   Appserver
+ * @package    TechDivision_ApplicationServer
+ * @subpackage Api
+ * @author     Tim Wagner <tw@techdivision.com>
+ * @author     Johann Zelger <jz@techdivision.com>
+ * @copyright  2013 TechDivision GmbH <info@techdivision.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link       http://www.appserver.io
+ */
+class VirtualHostNode extends AbstractParamsNode
+{
+
+    /**
+     * The virtual host name
+     *
+     * @var string
+     * @AS\Mapping(nodeType="string")
+     */
+    protected $name;
+
+    /**
+     * Returns the virtual hosts name
+     *
+     * @return string The server's type
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
