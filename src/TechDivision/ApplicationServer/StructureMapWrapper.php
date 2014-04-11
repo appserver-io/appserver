609b749c4a4a826d53f9960e3f4a093883552ac3
wick-ed <b.wick@techdivision.com>
2014-04-11 12:12:59 +0200

    added PBC wrapper for StructureMap class

---
+++ src/TechDivision/ApplicationServer/StructureMapWrapper.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @category   Appserver
+ * @package    TechDivision
+ * @subpackage ApplicationServer
+ * @author     Bernhard Wick <b.wick@techdivision.com>
+ * @copyright  2014 TechDivision GmbH - <info@techdivision.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php
+ *             Open Software License (OSL 3.0)
+ * @link       http://www.techdivision.com/
+ */
+
+namespace TechDivision\ApplicationServer;
+
+use TechDivision\PBC\StructureMap;
+use TechDivision\PBC\Config;
+
+/**
+ * TechDivision\ApplicationServer\StructureMap
+ *
+ * This class wraps PBC's StructureMap class to make it possible to have the map stored as a Stackable object.
+ * This is needed for memory and time efficient usage of the PBC loader.
+ *
+ * @category   Appserver
+ * @package    TechDivision
+ * @subpackage ApplicationServer
+ * @author     Bernhard Wick <b.wick@techdivision.com>
+ * @copyright  2014 TechDivision GmbH - <info@techdivision.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php
+ *             Open Software License (OSL 3.0)
+ * @link       http://www.techdivision.com/
+ */
+class StructureMapWrapper extends StructureMap
+{
+    /**
+     * The unique key to store the class map in the initial context.
+     *
+     * @var string
+     */
+    const CLASS_MAP = 'StructureMapWrapper.classMap';
+
+    /**
+     * @var \TechDivision\ApplicationServer\GenericStackable $map The actual container for the map
+     */
+    protected $stackableMap;
+
+    /**
+     * @var \TechDivision\ApplicationServer\InitialContext $initialContext Our initial context
+     */
+    protected $initialContext;
+
+    /**
+     * Default constructor
+     *
+     * @param array                                          $autoloaderPaths  Which paths do we include in our map?
+     * @param array                                          $enforcementPaths Which paths do we have to enforce
+     * @param \TechDivision\PBC\Config                       $config           Configuration
+     * @param \TechDivision\ApplicationServer\InitialContext $initialContext   Our initial context
+     */
+    public function __construct($autoloaderPaths, $enforcementPaths, Config $config, $initialContext)
+    {
+        // Save our initial context as we need it for loading
+        $this->initialContext = $initialContext;
+
+        // Call the parent constructor as we need the information
+        parent::__construct($autoloaderPaths, $enforcementPaths, $config);
+    }
+
+    /**
+     * Will load a serialized map from the storage file, if it exists
+     *
+     * @return bool
+     */
+    protected function load()
+    {
+        $this->stackableMap = $this->getInitialContext()->getAttribute(self::CLASS_MAP);
+
+        // Can we read the intended path?
+        if (!isset($this->stackableMap)) {
+
+            // If the map of our parent is empty we have to start generating it
+            if (empty($this->map)) {
+
+                // First of all try to use the parent load function, maybe we got it within a file.
+                // If not we have to start generating
+                if (!parent::load()) {
+
+                    // Start generation
+                    parent::generate();
+                }
+            }
+
+            // Get the map
+            $this->stackableMap = new GenericStackable();
+
+            // Copy the contents from the array into our stackable
+            foreach ($this->map as $key => $entry) {
+
+                $this->stackableMap[$key] = $entry;
+            }
+
+            // Remove the version entry from the map, we do not need it
+            unset($this->stackableMap['version']);
+
+            // Save the generated class into our storage
+            $this->getInitialContext()->setAttribute(self::CLASS_MAP, $this->stackableMap);
+
+            return true;
+
+        } else {
+
+            return true;
+        }
+    }
+
+    /**
+     * Simple getter for our initial context
+     *
+     * @return \TechDivision\ApplicationServer\InitialContext InitialContext
+     */
+    public function getInitialContext()
+    {
+        return $this->initialContext;
+    }
+}
