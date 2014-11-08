4640a026eadc98ec00d90bed06ab81c8e7d2abe6
wagnert <wagner_tim78@hotmail.com>
2014-11-08 19:11:15 +0100

    Add ProfileModule to integrate profile functionality

---
+++ src/TechDivision/ApplicationServer/Modules/ProfileModule.php
@@ -0,0 +1,160 @@
+<?php
+
+/**
+ * TechDivision\ApplicationServer\Modules\ProfileModule
+ *
+ * PHP version 5
+ *
+ * @category   Appserver
+ * @package    TechDivision_ApplicationServer
+ * @subpackage Modules
+ * @author     Tim Wagner <tw@techdivision.com>
+ * @copyright  2014 TechDivision GmbH <info@techdivision.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link       http://www.appserver.io
+ */
+
+namespace TechDivision\ApplicationServer\Modules;
+
+use TechDivision\Connection\ConnectionRequestInterface;
+use TechDivision\Connection\ConnectionResponseInterface;
+use TechDivision\Server\Dictionaries\ModuleHooks;
+use TechDivision\Server\Interfaces\ModuleInterface;
+use TechDivision\Server\Interfaces\RequestContextInterface;
+use TechDivision\Server\Interfaces\ServerContextInterface;
+
+/**
+ * Profile module that allows to send profile log messages
+ * via PSR-3 compatible logger implementation.
+ *
+ * @category   Appserver
+ * @package    TechDivision_ApplicationServer
+ * @subpackage Modules
+ * @author     Tim Wagner <tw@techdivision.com>
+ * @copyright  2014 TechDivision GmbH <info@techdivision.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link       http://www.appserver.io
+ */
+class ProfileModule implements ModuleInterface
+{
+
+    /**
+     * Defines the module name
+     *
+     * @var string
+     */
+    const MODULE_NAME = 'profile';
+
+    /**
+     * Hold's the server context instance
+     *
+     * @var \TechDivision\Server\Interfaces\ServerContextInterface
+     */
+    protected $serverContext;
+
+    /**
+     * Return's the request instance
+     *
+     * @return \TechDivision\Http\HttpRequestInterface The request instance
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    /**
+     * Returns the response instance
+     *
+     * @return \TechDivision\Http\HttpResponseInterface The response instance;
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+    /**
+     * Return's the server context instance
+     *
+     * @return \TechDivision\Server\Interfaces\ServerContextInterface
+     */
+    public function getServerContext()
+    {
+        return $this->serverContext;
+    }
+
+    /**
+     * Initiates the module
+     *
+     * @param \TechDivision\Server\Interfaces\ServerContextInterface $serverContext The server's context instance
+     *
+     * @return bool
+     * @throws \TechDivision\Server\Exceptions\ModuleException
+     */
+    public function init(ServerContextInterface $serverContext)
+    {
+        $this->serverContext = $serverContext;
+    }
+
+    /**
+     * Implements module logic for given hook.
+     *
+     * @param \TechDivision\Connection\ConnectionRequestInterface     $request        A request object
+     * @param \TechDivision\Connection\ConnectionResponseInterface    $response       A response object
+     * @param \TechDivision\Server\Interfaces\RequestContextInterface $requestContext A requests context instance
+     * @param int                                                     $hook           The current hook to process logic for
+     *
+     * @return bool
+     * @throws \TechDivision\Server\Exceptions\ModuleException
+     */
+    public function process(
+        ConnectionRequestInterface $request,
+        ConnectionResponseInterface $response,
+        RequestContextInterface $requestContext,
+        $hook
+    ) {
+        // In php an interface is, by definition, a fixed contract. It is immutable.
+        // So we have to declair the right ones afterwards...
+        /** @var $request \TechDivision\Http\HttpRequestInterface */
+        /** @var $request \TechDivision\Http\HttpResponseInterface */
+
+        // if false hook is comming do nothing
+        if (ModuleHooks::RESPONSE_POST !== $hook) {
+            return;
+        }
+
+        // set req and res object internally
+        $this->request = $request;
+        $this->response = $response;
+    }
+
+    /**
+     * Return's an array of module names which should be executed first
+     *
+     * @return array The array of module names
+     */
+    public function getDependencies()
+    {
+        return array();
+    }
+
+    /**
+     * Returns the module name
+     *
+     * @return string The module name
+     */
+    public function getModuleName()
+    {
+        return self::MODULE_NAME;
+    }
+
+    /**
+     * Prepares the module for upcoming request in specific context
+     *
+     * @return bool
+     * @throws \TechDivision\Server\Exceptions\ModuleException
+     */
+    public function prepare()
+    {
+        // nothing to prepare for this module
+    }
+}
