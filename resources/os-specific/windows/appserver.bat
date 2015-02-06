16387a67975a5b7efc6b7fca635dc344cb0b96b2
Tim Wagner <t.wagner@techdivision.com>
2015-02-06 08:52:35 +0100

    Delete files

--- resources/os-specific/windows/appserver.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-ECHO Starting appserver.io...
-
-set php=php\php.exe
-set ini=php\php.ini
-
-%php% -c %ini% -dappserver.php_sapi=appserver -dappserver.remove_functions=getenv,putenv server.php %*
