2077720b68f57816fc34ce4401129054172dd906
wickb <b.wick@techdivision.com>
2016-04-23 18:57:21 +0200

    made configuration file pickup depending on the app environment

---
+++ src/AppserverIo/Appserver/Core/Utilities/ConfigurationKeys.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * \AppserverIo\Appserver\Core\Utilities\ConfigurationKeys
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @author    Tim Wagner <tw@appserver.io>
+ * @copyright 2015 TechDivision GmbH <info@appserver.io>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/appserver-io/appserver
+ * @link      http://www.appserver.io
+ */
+
+namespace AppserverIo\Appserver\Core\Utilities;
+
+/**
+ * Utility class that contains keys for configuration mappings
+ *
+ * @author    Tim Wagner <tw@appserver.io>
+ * @copyright 2015 TechDivision GmbH <info@appserver.io>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/appserver-io/appserver
+ * @link      http://www.appserver.io
+ */
+class ConfigurationKeys
+{
+
+    /**
+     * Key for the 'app environment'.
+     *
+     * @var string APP_ENVIRONMENT
+     */
+    const APP_ENVIRONMENT = 'appserver.webapp.environment';
+
+    /**
+     * This is a utility class, so protect it against direct
+     * instantiation.
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * This is a utility class, so protect it against cloning.
+     *
+     * @return void
+     */
+    private function __clone()
+    {
+    }
+}
