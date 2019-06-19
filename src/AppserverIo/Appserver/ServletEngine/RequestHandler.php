8124f2f9d2019af87b258927f3799764debdcfab
Marcus Döllerer <pathmissing@users.noreply.github.com>
2019-06-19 22:26:46 +0200

    Remove exception flattening from RequestContext

--- src/AppserverIo/Appserver/ServletEngine/RequestHandler.php
+++ src/AppserverIo/Appserver/ServletEngine/RequestHandler.php
@@ -186,7 +186,6 @@ class RequestHandler extends \Thread
             }
 
         } catch (\Exception $e) {
-            ErrorUtil::singleton()->flattenExceptionBacktrace($e);
             $this->addError(ErrorUtil::singleton()->fromException($e));
         }
 
